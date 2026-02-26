e1ab644bc47c9f4bceb1ed1cadc4274015662960
mruchus <mnn23@ic.ac.uk>
2026-02-26 09:48:33 +0000

    just train on best params

--- classifier/trainer.py
+++ classifier/trainer.py
@@ -151,6 +151,8 @@ if __name__ == "__main__":
     param_combinations = list(itertools.product(lr_list, wd_list, accum_list, warmup_list, alpha_list))
     print(f"Total combinations: {len(param_combinations)}")
 
+    param_combinations = [(1e-6, 0.1, 2, 1500, 0.5)]
+
     best_f1_overall  = 0.0
     best_params      = None
     best_dev_preds   = None
